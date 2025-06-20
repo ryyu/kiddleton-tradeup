e7cc21226719a146f5a0c4abf483d4e222b0efe5
Ryan Yu <yuryan45@gmail.com>
2025-06-19 23:57:58 -0700

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true
   }
