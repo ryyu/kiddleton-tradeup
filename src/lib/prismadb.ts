141f25e8e904078ea530c9880319fcc9381f2ce7
Ryan Yu <yuryan45@gmail.com>
2025-06-19 22:45:52 -0700

    changed prisma client instance

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,10 +1,17 @@
 import { PrismaClient } from "../../generated/prisma/client";
 
-declare global {
-    var prisma: PrismaClient | undefined
-}
+// declare global {
+//     var prisma: PrismaClient | undefined
+// }
 
-const client = global.prisma || new PrismaClient()
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+// const client = global.prisma || new PrismaClient()
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
 
-export default client;
+// export default client;
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
